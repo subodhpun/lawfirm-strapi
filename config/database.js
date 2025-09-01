5394d8976bf64aaf959a7cdca46a95c2dadadc49
subodhpun <subodhmagar07@gmail.com>
2025-09-01 12:06:20 +0545

    more sll

--- config/database.js
+++ config/database.js
@@ -1,72 +1,43 @@
-const path = require('path');
-const fs = require('fs');
+const fs = require("fs");
+const path = require("path");
 
 module.exports = ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'sqlite');
-
-  // Path to optional custom CA certificate
-  const certPath = path.resolve(__dirname, '..', 'prod-ca-2021.crt');
-  const hasCert = fs.existsSync(certPath);
+  const client = env("DATABASE_CLIENT", "sqlite");
 
   const connections = {
     postgres: {
       connection: {
-        connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false)
-          ? hasCert
-            ? {
-                rejectUnauthorized: true,
-                ca: fs.readFileSync(certPath).toString(),
-              }
-            : {
-                rejectUnauthorized: true, // rely on system CA
-              }
-          : false,
-        schema: env('DATABASE_SCHEMA', 'public'),
-      },
-      pool: {
-        min: env.int('DATABASE_POOL_MIN', 2),
-        max: env.int('DATABASE_POOL_MAX', 10),
-      },
-    },
-
-    mysql: {
-      connection: {
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool(
-            'DATABASE_SSL_REJECT_UNAUTHORIZED',
-            true
-          ),
+        // Use DATABASE_URL directly; SSL will be inferred from sslmode=require
+        connectionString: env("DATABASE_URL"),
+        schema: env("DATABASE_SCHEMA", "public"),
+        // Optional: if you want explicit SSL using a CA
+        ssl: {
+          rejectUnauthorized: true,
+          // If you have a CA cert, you can provide it via ENV:
+          ca: env("DATABASE_CA") || undefined,
         },
       },
       pool: {
-        min: env.int('DATABASE_POOL_MIN', 2),
-        max: env.int('DATABASE_POOL_MAX', 10),
+        min: env.int("DATABASE_POOL_MIN", 2),
+        max: env.int("DATABASE_POOL_MAX", 10),
       },
     },
-
+    mysql: {
+      connection: {
+        host: env("DATABASE_HOST", "localhost"),
+        port: env.int("DATABASE_PORT", 3306),
+        database: env("DATABASE_NAME", "strapi"),
+        user: env("DATABASE_USERNAME", "strapi"),
+        password: env("DATABASE_PASSWORD", "strapi"),
+        ssl: env.bool("DATABASE_SSL", false)
+          ? { rejectUnauthorized: env.bool("DATABASE_SSL_REJECT_UNAUTHORIZED", true) }
+          : false,
+      },
+      pool: { min: env.int("DATABASE_POOL_MIN", 2), max: env.int("DATABASE_POOL_MAX", 10) },
+    },
     sqlite: {
       connection: {
-        filename: path.join(
-          __dirname,
-          '..',
-          env('DATABASE_FILENAME', '.tmp/data.db')
-        ),
+        filename: path.join(__dirname, "..", env("DATABASE_FILENAME", ".tmp/data.db")),
       },
       useNullAsDefault: true,
     },
@@ -76,10 +47,7 @@ module.exports = ({ env }) => {
     connection: {
       client,
       ...connections[client],
-      acquireConnectionTimeout: env.int(
-        'DATABASE_CONNECTION_TIMEOUT',
-        60000
-      ),
+      acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
     },
   };
 };
