17e57ec036fe04f9fe38613f70e78e3aad586f88
subodhpun <subodhmagar07@gmail.com>
2025-09-01 12:22:09 +0545

    dic

--- config/database.js
+++ config/database.js
@@ -2,38 +2,24 @@ const fs = require("fs");
 const path = require("path");
 
 module.exports = ({ env }) => {
-  const client = env("DATABASE_CLIENT", "sqlite");
+  const client = env("DATABASE_CLIENT", "postgres");
+
+  const caPath = path.join(__dirname, "../config/prod-ca-2021.crt"); // path to your cert
 
   const connections = {
     postgres: {
       connection: {
-        // Use DATABASE_URL directly; SSL will be inferred from sslmode=require
         connectionString: env("DATABASE_URL"),
         schema: env("DATABASE_SCHEMA", "public"),
-        // Optional: if you want explicit SSL using a CA
         ssl: {
           rejectUnauthorized: true,
-          // If you have a CA cert, you can provide it via ENV:
-          ca: env("DATABASE_CA") || undefined,
+          ca: fs.existsSync(caPath) ? fs.readFileSync(caPath).toString() : undefined,
         },
       },
       pool: {
         min: env.int("DATABASE_POOL_MIN", 2),
         max: env.int("DATABASE_POOL_MAX", 10),
       },
-    },
-    mysql: {
-      connection: {
-        host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 3306),
-        database: env("DATABASE_NAME", "strapi"),
-        user: env("DATABASE_USERNAME", "strapi"),
-        password: env("DATABASE_PASSWORD", "strapi"),
-        ssl: env.bool("DATABASE_SSL", false)
-          ? { rejectUnauthorized: env.bool("DATABASE_SSL_REJECT_UNAUTHORIZED", true) }
-          : false,
-      },
-      pool: { min: env.int("DATABASE_POOL_MIN", 2), max: env.int("DATABASE_POOL_MAX", 10) },
     },
     sqlite: {
       connection: {
